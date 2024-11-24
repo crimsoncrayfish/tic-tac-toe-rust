0c7e1ce17e9d21a8177eadd8a148f0d4e623a9f3
crayfish <m1992steen@gmail.com>
2024-11-24 21:30:31 +0200

    env args

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use core::time;
+use std::{env, usize};
 
 pub mod conways_game;
 pub mod conways_law;
 pub mod terminal_formatter;
 
 fn main() {
-    let mut gs = conways_game::ConwaysGame::init(10, 10, 10);
+    let args: Vec<String> = env::args().collect();
+
+    let x_len: usize = args[1].parse().expect("Not a valid number");
+    let y_len: usize = args[2].parse().expect("Not a valid number");
+    let seed: u64 = args[3].parse().expect("Not a valid number");
+    let mut gs = conways_game::ConwaysGame::init(x_len, y_len, seed);
     let duration = time::Duration::from_millis(1000);
     gs.run(duration);
 }
