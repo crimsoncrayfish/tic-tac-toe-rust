a1117ce25842a73b58e75fc610819bf4a01b91b5
crayfish <m1992steen@gmail.com>
2024-11-27 07:42:25 +0200

    refactored cmd args collecting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use core::time;
-use std::{env, usize};
+use std::{env, str::FromStr, usize};
 
 use conways_game::PrintMode;
 
@@ -10,49 +10,32 @@ pub mod terminal_formatter;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let mut x_len: usize = 10;
-    let mut y_len: usize = 10;
-    let mut seed: u64 = 10;
-    let mut print_mode: PrintMode = PrintMode::PRETTY;
+    let x_len: usize = read_config(&args, "--x-len".to_string(), 10);
+    let y_len: usize = read_config(&args, "--y-len".to_string(), 10);
+    let seed: u64 = read_config(&args, "--seed".to_string(), 10);
+    let print_mode: PrintMode = read_config(&args, "--mode".to_string(), PrintMode::PRETTY);
 
-    for i in 1..args.len() {
-        match args[i].as_str() {
-            "--x-len" => {
-                if let Some(val) = args.get(i + 1) {
-                    x_len = val.parse().unwrap_or_else(|_| {
-                        eprint!("invalid value for --x-len. Using default.");
-                        x_len
-                    });
-                }
-            }
-            "--y-len" => {
-                if let Some(val) = args.get(i + 1) {
-                    y_len = val.parse().unwrap_or_else(|_| {
-                        eprint!("invalid value for --y-len. Using default.");
-                        x_len
-                    });
-                }
-            }
-            "--seed" => {
-                if let Some(val) = args.get(i + 1) {
-                    seed = val.parse().unwrap_or_else(|_| {
-                        eprint!("invalid value for --seed. Using default.");
-                        seed
-                    });
-                }
-            }
-            "--mode" => {
-                if let Some(val) = args.get(i + 1) {
-                    print_mode = val.parse().unwrap_or_else(|_| {
-                        eprint!("invalid value for --mode. Using default.");
-                        print_mode
-                    });
-                }
-            }
-            _ => {}
-        }
-    }
     let mut gs = conways_game::ConwaysGame::init(x_len, y_len, seed);
     let duration = time::Duration::from_millis(1000);
     gs.run(duration, print_mode);
+}
+
+fn read_config<T>(args: &Vec<String>, arg: String, default: T) -> T
+where
+    T: FromStr + Clone + Copy,
+{
+    let mut out: T = default.clone();
+    for i in 1..args.len() {
+        if args[i].as_str() != arg {
+            continue;
+        }
+        if let Some(val) = args.get(i + 1) {
+            out = val.parse().unwrap_or_else(|_| {
+                eprint!("invalid value for {}. Using default.", arg);
+                default
+            });
+        }
+    }
+
+    out
 }
