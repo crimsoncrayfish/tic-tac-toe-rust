e7aa8250ec462144126ad5e8ae727166db06a1a6
marco.steenkamp <m1992steen@gmail.com>
2025-01-02 19:12:32 +0200

    a start

--- src/rendering/render_object.rs
+++ src/rendering/render_object.rs
@@ -2,48 +2,35 @@ use std::io::Error;
 
 use crate::{
     handler::handle::Handle,
-    shared::{
-        shared_errors::SharedErrors,
-        square::Square,
-        usize2d::{Coord, Usize2d},
-    },
+    shared::{shared_errors::SharedErrors, square::Square, usize2d::Coord},
 };
 
 use super::sprite::Sprite;
 
 pub struct RenderObject {
-    coordinate: Usize2d,
+    coordinate: Coord,
     sprite: Sprite,
 }
 impl RenderObject {
-    pub fn new(sprite: Sprite, location: Coord) -> Self {
+    pub fn new(sprite: Sprite, coord: Coord) -> Self {
         RenderObject {
             sprite,
-            coordinate: location,
+            coordinate: coord,
         }
     }
     pub fn get_area(&self) -> Square {
         Square::new(
-            Usize2d::new(self.coordinate.x, self.coordinate.y),
-            Usize2d::new(
+            Coord::new(self.coordinate.x, self.coordinate.y),
+            Coord::new(
                 self.coordinate.x + self.sprite.width - 1,
                 self.coordinate.y + self.sprite.width - 1,
             ),
         )
     }
-    pub fn get_location(&self) -> Usize2d {
+    pub fn get_location(&self) -> Coord {
         self.coordinate
     }
-    pub fn write_clamped(
-        &self,
-        _handle: &mut dyn Handle,
-        _clamp: Square,
-    ) -> Result<usize, std::io::Error> {
-        // TODO:let to_write = self.sprite.get_content_for_area(clamp, self.coordinate);
 
-        //handle.write(&to_write)
-        Err(Error::new(std::io::ErrorKind::Other, "TBI"))
-    }
     /// Get the content that should be written to the screen given the limitations i.t.o
     /// coordinates and available screen space
     ///
